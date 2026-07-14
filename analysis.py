fa6a11eb526c564203cadd5f2e4cad8313806304
Jamie Lin <jamielin920421@gmail.com>
2026-07-14 15:07:05 -0600

    Added the code for the largest sale

--- analysis.py
+++ analysis.py
@@ -2,3 +2,5 @@ import pandas as pd
 df = pd.read_csv("Date.csv")
 print("Number of rows:", len(df))
 print("Mean price:", df["price"].mean())
+# Print the largest sale:
+print("Largest Sale:", df["Price"].max())
